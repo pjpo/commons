458b59d7c82c950c80e9132c25167ff8247aa84f
jean philippe delabre <jeanphilippe.delabre@gmail.com>
2014-07-17 22:18:35 +0200

    initial import

---
+++ commons/ioutils/src/main/java/com/github/pjpo/commons/ioutils/App.java
@@ -0,0 +1,13 @@
+package com.github.pjpo.commons.ioutils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 2!" );
+    }
+}
